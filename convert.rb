c023f6472a5361fb3510660d50b68700e492f136
R. Tyler Croy <tyler@monkeypox.org>
2011-10-28 17:59:38 -0700

    Derp

---
+++ convert.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'grit'
+
+def walk_tree(tree)
+  tree.contents.each do |subtree|
+    if subtree.instance_of? Grit::Tree
+      walk_tree(subtree)
+    else
+      puts "#{subtree.id} - #{subtree.name} - #{subtree.mode} - #{subtree.size}"
+    end
+  end
+end
+
+def run
+  repo = Grit::Repo.new(File.dirname(__FILE__))
+  repo.commits.each do |commit|
+    shortname = commit.author.email.split("@").first
+    puts "#{shortname} - #{commit.id}"
+    walk_tree(commit.tree)
+    puts "----"
+  end
+end
+
+run
