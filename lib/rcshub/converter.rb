b64fd7e3080bb84c29a6e65068275e596c7f69a9
R. Tyler Croy <tyler@monkeypox.org>
2011-10-29 01:17:03 -0700

    Tidy/speed up the importer by separating out the creation of commits so it can be threaded
    While this doesn't add a huge speed boost, under JRuby, when running with threadpooling and a minimum
    pool size, it nets between 5-10% improvements over non-threaded MRI
    
    (running with: `jruby -J-Djruby.thread.pooling=true -J-Djruby.thread.pool.min=10 ~/source/github/rcshub/import.rb`)

--- lib/rcshub/converter.rb
+++ lib/rcshub/converter.rb
@@ -7,10 +7,16 @@ require 'temp_dir'
 module RCSHub
   class Converter
     attr_accessor :count
+    attr_accessor :work_dir
+    attr_accessor :threaded
 
-    def initialize(opts=nil)
-      opts ||= {}
+    def initialize(opts={})
       root = opts[:root] || Dir.pwd
+      @threaded = true
+      if opts.has_key? :threaded
+       @threaded = opts[:threaded]
+      end
+
       @repo = Grit::Repo.new(root)
       @work_dir = TempDir.create(:rootpath => root)
       @count = 0
@@ -19,47 +25,93 @@ module RCSHub
     end
 
     def walk_tree(commit, file_path, tree, seen)
+      threads = []
       tree.contents.each do |subtree|
+
         if subtree.instance_of? Grit::Tree
           tree_path = "#{file_path}/#{subtree.name}"
           FileUtils.mkdir_p(tree_path + "/RCS")
-          walk_tree(commit, "#{file_path}/#{subtree.name}", subtree, seen)
+          if @threaded
+            threads << Thread.new do
+              walk_tree(commit, "#{file_path}/#{subtree.name}", subtree, seen)
+            end
+          else
+            walk_tree(commit, "#{file_path}/#{subtree.name}", subtree, seen)
+          end
           next
         end
-
 
         unless seen[subtree.id]
           seen[subtree.id] = true
 
-          object_path = "#{file_path}/#{subtree.name}"
+          if subtree.size == 0
+            next
+          end
 
-          message = "#{commit.message}\n\nrcshub: #{commit.id}"
-
-          unless subtree.size <= 0
-            fd = File.new(object_path, 'w')
-            fd.write(subtree.data)
-            fd.close
-            print %x{echo "#{message}" | ci -l -w#{@author} "#{object_path}"}
+          if @threaded
+            threads << Thread.new do
+              create_commit(file_path, commit, subtree)
+            end
           else
-            puts "#{subtree.name} empty, skipping"
+            create_commit(file_path, commit, subtree)
           end
         end
+      end
+
+      threads.each do |t|
+        t.join
       end
     end
 
     def convert
       rcs_path = @work_dir + "/RCS"
 
-      puts "Using work directory: #{@work_dir}"
       Dir.mkdir(rcs_path)
+      # For bigger repositories, this is going to end up using lots of memory
       seen = {}
 
       @repo.commits('master', false).reverse_each do |commit|
         @count += 1
         @author = commit.author.email.split("@").first
         walk_tree(commit, @work_dir, commit.tree, seen)
-        puts "----------------"
       end
+    end
+
+    private
+
+    def create_commit(root_path, commit, subtree)
+      object_path = "#{root_path}/#{subtree.name}"
+
+      message = "#{commit.message}\n\nrcshub: #{commit.id}"
+
+      fd = File.new(object_path, 'w')
+      fd.write(subtree.data)
+      fd.close
+
+      # Creating a tempfile so I can cat it later in the backticks. echo(1)
+      # can't handle all the kinds of stupid characters that folks put into
+      # commit messages
+      #
+      # It also seems that I cannot use IO.popen or Open3.popen3 with ci(1) for
+      # some reason, broken pipes and bad exit statuses all over the place.
+      #
+      # Very odd
+      tf = Tempfile.new("rcs-commit")
+      tf.write message
+      tf.flush
+
+      `cat #{tf.path} | ci -l -w#{@author} \"#{object_path}\"`
+      unless $?.exitstatus == 0
+        puts $?
+        puts "BAD EXIT"
+        puts subtree.name
+        puts message
+        puts "----"
+        exit 1
+      end
+
+      tf.close
+      tf.unlink
     end
   end
 end
