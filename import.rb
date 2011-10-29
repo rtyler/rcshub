b8fd6dd1fccd1460880485fc3f36a0ec20144cb7
R. Tyler Croy <tyler@monkeypox.org>
2011-10-29 01:18:20 -0700

    Print out whether or not the import has run threaded or not

--- import.rb
+++ import.rb
@@ -4,7 +4,12 @@ require 'rubygems'
 
 require File.dirname(__FILE__) + "/lib/rcshub/converter.rb"
 
-converter = RCSHub::Converter.new(:root => Dir.pwd)
+converter = RCSHub::Converter.new({:root => Dir.pwd})
 converter.convert
-puts "Converted #{converter.count} commits"
+puts "Converted #{converter.count} commits into #{converter.work_dir}"
+if converter.threaded
+  puts "used threads"
+else
+  puts "single-threaded"
+end
 
