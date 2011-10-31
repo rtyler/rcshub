98dc321612942a562c8c04d6df2b381e66092bef
R. Tyler Croy <tyler@monkeypox.org>
2011-10-31 13:52:31 -0700

    I don't trust this threading code as much as I'd like

--- import.rb
+++ import.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 
 require File.dirname(__FILE__) + "/lib/rcshub/converter.rb"
 
-converter = RCSHub::Converter.new({:root => Dir.pwd})
+converter = RCSHub::Converter.new({:root => Dir.pwd, :threaded => false})
 converter.convert
 puts "Converted #{converter.count} commits into #{converter.work_dir}"
 if converter.threaded
