ac384c0e5eb376257d38aac690f53e8e13871304
R. Tyler Croy <tyler@monkeypox.org>
2011-10-31 23:06:02 -0700

    Add a counter to make sure we're showing enough repository rows on the profile

--- features/step_definitions/profile.rb
+++ features/step_definitions/profile.rb
@@ -20,8 +20,8 @@ When /^I visit the user's profile$/ do
   visit "/#{@username}"
 end
 
-Then /^I should see (\d+) repositories$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see (\d+) repositories$/ do |count|
+  response_body.should have_selector(".repo_row", :count => count.to_i)
 end
 
 Given /^a user with (\d+) timeline events$/ do |arg1|
